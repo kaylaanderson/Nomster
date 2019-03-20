fc8e08db93c8c3b8c8f6adc040bf7622edfc1d5b
Kayla Anderson <kaylanicolette@live.com>
2019-03-20 14:45:43 +0000

    Building a Form to Save New Places

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,7 +1,11 @@
 class PlacesController < ApplicationController
 
   def index
-    Place.paginate(:page => params[:page], :per_page => 10)
+    @places = Place.all
+  end
+
+  def new
+    @place = Place.new
   end
 
 end
